10d44c845a2058e1190f3988de2abaa410bf2354
ZhufuZebao <1176354795@qq.com>
2022-10-14 13:34:29 +0800

    redux数据持久化

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,25 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore,combineReducers} from '@reduxjs/toolkit'
+import {persistStore,persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import globalSlice from './reducers/globalReducer'
 import authSlice from './reducers/authReducer'
 
+const config = {
+    key: 'root',
+    storage:storage,
+}
+
 const store = configureStore({
-    reducer:{
+    reducer:persistReducer(config,combineReducers({
         authSlice,
         globalSlice
-    }
+    })),
+    middleware:(getDefaultMiddleware:any) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        })
 })
+persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export default  store
