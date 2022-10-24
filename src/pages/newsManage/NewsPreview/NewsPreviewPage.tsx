f6309873d6401c64b58836eb14bf250cf53807e3
ZhufuZebao <1176354795@qq.com>
2022-10-24 15:31:22 +0800

    草稿详情

--- src/pages/newsManage/NewsPreview/NewsPreviewPage.tsx
+++ src/pages/newsManage/NewsPreview/NewsPreviewPage.tsx
@@ -3,15 +3,19 @@ import {useParams} from 'react-router-dom'
 import { Button, Descriptions, PageHeader } from 'antd';
 import {initialize} from "../../../server/server";
 import {timestampToTime} from "../../../util/function";
+import style from './NewsPreviewPage.module.css'
 
 function NewsPreviewPage() {
     useEffect(() => {
         initialize(`/news/${params.id}?_expand=category&_expand=role`).then(res => {
+            console.log(res.data)
             setShowData(res.data)
         })
     },[])
     const params = useParams()
     const [showData,setShowData] = useState<any>({})
+    const auditList = ['未审核','审核中','已通过','未通过']
+    const publicList = ['未发布','待发布','已上线','已下线']
     return (
         <div>
             <PageHeader
@@ -24,14 +28,14 @@ function NewsPreviewPage() {
                     <Descriptions.Item label="创建时间">{timestampToTime(showData.createTime)}</Descriptions.Item>
                     <Descriptions.Item label="发布时间">{showData.publishTime?timestampToTime(showData.publishTime):'-'}</Descriptions.Item>
                     <Descriptions.Item label="区域">{showData.region}</Descriptions.Item>
-                    <Descriptions.Item label="审核状态">Lili Qu</Descriptions.Item>
-                    <Descriptions.Item label="发布状态">Lili Qu</Descriptions.Item>
-                    <Descriptions.Item label="访问数量">{showData.view}</Descriptions.Item>
-                    <Descriptions.Item label="点赞数量">{showData.star}</Descriptions.Item>
-                    <Descriptions.Item label="评论数量">0</Descriptions.Item>
-
+                    <Descriptions.Item label="审核状态" style={{color:"red",fontWeight:600}}>{auditList[showData.auditState]}</Descriptions.Item>
+                    <Descriptions.Item label="发布状态" style={{color:"red",fontWeight:600}}>{publicList[showData.publishState]}</Descriptions.Item>
+                    <Descriptions.Item label="访问数量" style={{color:"green"}}>{showData.view}</Descriptions.Item>
+                    <Descriptions.Item label="点赞数量" style={{color:"green"}}>{showData.star}</Descriptions.Item>
+                    <Descriptions.Item label="评论数量" style={{color:"green"}}>0</Descriptions.Item>
                 </Descriptions>
             </PageHeader>
+          <div dangerouslySetInnerHTML={{__html:showData.content}} className={style.showContent}></div>
         </div>
     );
 }
